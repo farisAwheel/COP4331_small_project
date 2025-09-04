e45a863cc94dda1fdd6539f25b447196080360a4
Faris Abdelrahem <farishabdelrahem@gmail.com>
2025-09-04 15:19:19 -0400

    Add JSON formation and return value for api requests

--- LAMPAPI/register.php
+++ LAMPAPI/register.php
@@ -7,7 +7,7 @@ $email = $data["email"];
 // attempt to connec to db
 $conn = new mysqli("localhost", "user", "L@MPGroup1A", "contact_manager"); 
 if($conn->connect_error) {
-    returnError($conn->connect_error);
+    returnError($conn->connect_error, 500);
 }
 else {
     $stmt = $conn->prepare("INSERT INTO users (username, password, email) VALUES(?, ?, ?)");
@@ -15,5 +15,26 @@ else {
     $stmt->execute();
     $stmt->close();
     $conn->close();
-    returnError("");
+    returnError("", 200);
+}
+
+function jsonify($obj, $status=200){
+    header('Content-type: application/json');
+    $json = json_encode($obj);
+    if($json === false){
+        $status = 500;
+        $json = json_encode([
+           "id" => 0,
+           "email" => "",
+           "error" => "JSON encoding failed" 
+        ]);
+    }
+    http_response_code($status);
+    echo $json;
+}
+function returnError($err, $status=400) {
+    $retValue = ([
+        "error" => $err
+    ]);
+    jsonify($retValue, $status);
 }
