c8175facaf70eb1e7c5a753cccbda3db0d940e9f
Faris Abdelrahem <farishabdelrahem@gmail.com>
2025-09-21 17:33:36 -0400

    add update api endpoint

---
+++ LAMPAPI/updateContact.php
@@ -0,0 +1,43 @@
+<?php
+// app sends a user id and a search query, api returns list of contacts
+$data = json_decode(file_get_contents("php://input"), true); // retrieve body from POST request
+$userid = $data["user_id"];
+$id = $data["id"];
+$name = $data["name"];
+$email = $data["email"];
+$phone = $data["phone"];
+
+
+$conn = new mysqli("localhost", "user", "L@MPGroup1A", "contact_manager"); 
+if($conn->connect_error) {
+    returnError($conn->connect_error, 500);
+}
+else {
+    $stmt = $conn->prepare("UPDATE contacts SET name = ?, email = ?, phone = ? WHERE id = ? AND user_id = ?");
+    $stmt->bind_param("sssii", $name, $email, $phone, $id, $userid);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    returnError("", 200);
+}
+
+function jsonify($obj, $status=200){
+    header('Content-type: application/json');
+    $json = json_encode($obj);
+    if($json === false){
+        $status = 500;
+        $json = json_encode([
+           "id" => 0,
+           "email" => "",
+           "error" => "JSON encoding failed" 
+        ]);
+    }
+    http_response_code($status);
+    echo $json;
+}
+function returnError($err, $status=400) {
+    $retValue = ([
+        "error" => $err
+    ]);
+    jsonify($retValue, $status);
+}
